34da172b0dd1a71e3d98af786eab747aa64061f3
Ujjal Suttra Dhar <usuttra@yahoo.com>
2016-07-04 17:19:26 +0600

    Updated comments

--- src/AbstractFactory/AbstractFactoryMain.java
+++ src/AbstractFactory/AbstractFactoryMain.java
@@ -1,15 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package AbstractFactory;
 
 import Common.Log;
 
 /**
  *
- * @author ujjal
+ * @author Ujjal Suttra Dhar <self@ujjal.net>
+ * @date Jul 4, 2016
  */
 public class AbstractFactoryMain {
-    
     public static void main(){
         Log.Verbose("Abstract Factory");
     }
-    
 }
